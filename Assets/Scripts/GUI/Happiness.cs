8464baac4992e4a0797de251d8d6c71520bb7f29
navidmatin <seyedmatin.1@osu.edu>
2015-01-17 11:05:26 -0500

    Added the happiness to the GUI

---
+++ Assets/Scripts/GUI/Happiness.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Happiness : MonoBehaviour {
+
+    public Sprite Ecstatic;
+    public Sprite Happy;
+    public Sprite Neutral;
+    public Sprite Sad;
+    public Sprite Depressed;
+    public Image currentImage;
+	// Use this for initialization
+	void Start () {
+        currentImage = this.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mood = ThirdWorldManager.Instance.CurrentMood;
+
+
+        switch(mood)
+        {
+            case(ThirdWorldManager.Mood.Ecstatic):
+                currentImage.sprite = Ecstatic;
+                break;
+            case(ThirdWorldManager.Mood.Happy):
+                currentImage.sprite = Happy;
+                break;
+            case (ThirdWorldManager.Mood.Neutral):
+                currentImage.sprite = Neutral;
+                break;
+            case (ThirdWorldManager.Mood.Sad):
+                currentImage.sprite = Sad;
+                break;
+            case (ThirdWorldManager.Mood.Depressed):
+                currentImage.sprite = Depressed;
+                break;           
+        }
+
+	}
+}
