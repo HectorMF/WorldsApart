a52167b46985c91069acb57b18a3ba9adbf9c321
Ryan Bone <rbone@covermymeds.com>
2015-01-17 13:08:15 -0500

    create generic use water handler

---
+++ Assets/Scripts/Handlers/ThirdWorldHandlers/UseWaterHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using WorldsApart.Interfaces;
+
+namespace WorldsApart.Handlers
+{
+	public class UseWaterHandler : Handler 
+	{
+		public override void invoke()
+		{
+			if (ThirdWorldManager.Instance.AnyWater && ThirdWorldManager.Instance.TryAction())
+			{
+				gameObject.GetComponent<Thirst>().Drink();
+			}
+			ThirdWorldManager.Instance.Report();
+		}
+	}
+}
