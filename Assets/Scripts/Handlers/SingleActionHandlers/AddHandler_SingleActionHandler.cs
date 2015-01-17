67299262741fd776b05f80e8ef28e161d3c0fde0
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-01-17 08:05:32 -0500

    Add Handler

---
+++ Assets/Scripts/Handlers/SingleActionHandlers/AddHandler_SingleActionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WorldsApart.Clickables;
+
+namespace WorldsApart.Handlers
+{
+    public class AddHandler_SingleActionHandler : SingleActionHandler
+    {
+        public Handler handler;
+        public Clickable target;
+
+        public override void innerDelegate()
+        {
+            target.AddHandler(handler);
+        }
+    }
+}
