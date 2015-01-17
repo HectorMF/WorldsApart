1ac8e9dd4736553b938cc020b3a97057506583cc
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-01-17 07:52:27 -0500

    Added new SingleActionHandler
    This is a wrapper on the Handler class and only allows the handler to be
    invoked once. SingleActionHandlers should inherit from
    SingleActionHandler and should override the innerDelegate function.

---
+++ Assets/Scripts/Handlers/SingleActionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorldsApart.Handlers
+{
+    public abstract class SingleActionHandler : Handler
+    {
+        private bool enabled = true;
+
+        public override void invoke()
+        {
+            if (enabled == true)
+            {
+                innerDelegate();
+                enabled = false;
+            }
+        }
+
+        public abstract void innerDelegate();
+    }
+}
