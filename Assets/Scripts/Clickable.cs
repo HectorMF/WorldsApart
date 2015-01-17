7a7faa3751db15e7a48aa0b8b65c3306ff530375
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-01-16 19:51:06 -0500

    Committing clicking stuff

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace WorldsApart
+{
+
+    public class Clickable : MonoBehaviour
+    {
+        public List<Handler> Handlers;
+
+        void OnMouseUpAsButton()
+        {
+            foreach(Handler handler in Handlers)
+                handler.invoke();
+        }
+    }
+
+}
