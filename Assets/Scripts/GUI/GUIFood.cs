84263457b4c870e7689853ebe89a58a8f66e3104
navidmatin <seyedmatin.1@osu.edu>
2015-01-17 12:01:46 -0500

    Updated Food GUI

---
+++ Assets/Scripts/GUI/GUIFood.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIFood : MonoBehaviour {
+
+    private Text _food;
+	// Use this for initialization
+	void Start () {
+        _food = this.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var foodtext = string.Format("{0}/{1} Food", ThirdWorldManager.Instance.CurrentFood, ThirdWorldManager.Instance.RequiredFood);
+        _food.text = foodtext;
+	}
+}
