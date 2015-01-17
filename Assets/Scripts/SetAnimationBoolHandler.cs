4403dc7781ec4aa018ba2b18c7e2780f36a37446
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-01-16 20:31:39 -0500

    AnimationTransitions

---
+++ Assets/Scripts/SetAnimationBoolHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace WorldsApart
+{
+    public class SetAnimationBoolHandler : Handler
+    {
+        public string booleanName;
+        public bool value;
+
+        public override void invoke()
+        {
+            var animator = gameObject.GetComponent<Animator>();
+            if (animator != null) animator.SetBool(booleanName, value);
+        }
+    }
+}
