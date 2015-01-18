218ac589eee84a2534c930ecd819151ebf1aed26
Hector Medina-Fetterman <javi@medina.net>
2015-01-18 11:39:04 -0500

    chane
    scene change

--- Assets/Scripts/GUI/GUIWater.cs
+++ Assets/Scripts/GUI/GUIWater.cs
@@ -4,17 +4,22 @@ using UnityEngine.UI;
 
 public class GUIWater : MonoBehaviour {
 
+    private float oldValue;
+    private float currentValue;
     private Slider slider;
+
 	// Use this for initialization
 	void Start () {
         slider = this.GetComponent<Slider>();
-        if(slider!=null)
+        if(slider != null)
             slider.maxValue = ThirdWorldManager.Instance.AvailableWater;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-        slider.value = ThirdWorldManager.Instance.CurrentWater;
+        if (currentValue != ThirdWorldManager.Instance.CurrentWater)
+        {
+            slider.maxValue = ThirdWorldManager.Instance.AvailableWater;
+        }
 	}
 }
