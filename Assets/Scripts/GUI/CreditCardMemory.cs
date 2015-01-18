2b7ef78e525afb8e2e3cc5b947bcc31c0860ed60
navidmatin <seyedmatin.1@osu.edu>
2015-01-18 01:45:57 -0500

    Added computer game

---
+++ Assets/Scripts/GUI/CreditCardMemory.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class CreditCardMemory : MonoBehaviour {
+    public string LastFourDigits = "4212";
+
+	public bool CheckLast4Digits(string digits)
+    {
+        if (digits == null)
+            return false;
+        Debug.Log(digits.ToString());
+        if(digits.Length!=4)
+        {
+            return false;    
+        }
+        if(digits==LastFourDigits)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void DoStuffBasedOnInput()
+    {
+        try
+        {
+            string digits = this.GetComponent<InputField>().text;
+            if(CheckLast4Digits(digits))
+            {
+                //TODO:Stuff Happens
+            }
+        }
+        catch(Exception e)
+        {
+            Debug.LogException(e);
+        }
+        
+    }
+}
