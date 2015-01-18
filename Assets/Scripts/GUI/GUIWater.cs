495e5137530166281ef72b07b105c66321475012
Hector Medina-Fetterman <javi@medina.net>
2015-01-18 11:52:27 -0500

    Water Easing

--- Assets/Scripts/GUI/GUIWater.cs
+++ Assets/Scripts/GUI/GUIWater.cs
@@ -3,23 +3,43 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GUIWater : MonoBehaviour {
+    public float easeTime = 1;
 
     private float oldValue;
     private float currentValue;
     private Slider slider;
-
+    private float easeTimer;
+    private bool easing;
 	// Use this for initialization
 	void Start () {
         slider = this.GetComponent<Slider>();
         if(slider != null)
             slider.maxValue = ThirdWorldManager.Instance.AvailableWater;
+        currentValue = slider.value;
+        oldValue = slider.value;
+        easing = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (currentValue != ThirdWorldManager.Instance.CurrentWater)
+        if (!easing && slider.value != ThirdWorldManager.Instance.CurrentWater)
         {
-            slider.maxValue = ThirdWorldManager.Instance.AvailableWater;
+            oldValue = slider.value;
+            currentValue = ThirdWorldManager.Instance.CurrentWater;
+            easeTimer = 0;
+            easing = true;
+        }
+
+        if (easing)
+        {
+            easeTimer += Time.deltaTime / easeTime;
+            if (easeTimer > 1)
+            {
+                easing = false;
+                slider.value = ThirdWorldManager.Instance.CurrentWater;
+            }
+            else
+                slider.value = Mathf.Lerp(oldValue, currentValue, easeTimer);
         }
 	}
 }
