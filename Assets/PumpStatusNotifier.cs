65ee6f8dc0dcaf443e8f53cd86be4438dda3c258
Ryan Bone <rbone@covermymeds.com>
2015-01-18 15:37:48 -0500

    Pump notifications

---
+++ Assets/PumpStatusNotifier.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PumpStatusNotifier : MonoBehaviour {
+
+	GameObject available, empty;
+	// Use this for initialization
+	void Start () {
+		available = transform.Find("PumpAvailable").gameObject;
+		available.SetActive(true);
+	}
+
+	void OnEnable()
+	{
+		ThirdWorldManager.OnDayEnd += DayEnd;
+	}
+	
+	void OnDisable()
+	{
+		ThirdWorldManager.OnDayEnd -= DayEnd;
+	}
+
+	void Update()
+	{
+		if (ThirdWorldManager.Instance.AvailableWater == 0)
+		{
+			available.SetActive(false);
+		}
+	}
+
+	void DayEnd()
+	{
+		available.SetActive(true);
+	}
+}
