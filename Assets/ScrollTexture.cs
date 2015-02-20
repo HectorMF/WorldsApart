f72ec10a5b14cd62b09a424230fc22a07f64618d
Hector Medina-Fetterman <javi@medina.net>
2015-02-20 17:30:22 -0500

    TestScene and shaders

---
+++ Assets/ScrollTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollTexture : MonoBehaviour {
+    public float speed;
+    void Update()
+    {
+        //Offset += Scroll * Time.deltaTime;
+        renderer.material.mainTextureOffset = new Vector2((Time.time * speed) % 1, 0f);
+        //renderer.material.SetTextureOffset("_MainTex", Offset);
+    }
+}
