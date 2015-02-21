026fa391c3b3c22a6e7cb0798cae34ee2ac3091c
Hector Medina-Fetterman <javi@medina.net>
2015-02-21 04:03:08 -0500

    DOTween and testing having multiple views per zone

---
+++ Assets/FrameAnchors.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using System.Collections.Generic;
+
+public class FrameAnchors : MonoBehaviour {
+    public Ease easingFunction = Ease.InBack;
+    public float duration;
+    public int index;
+    private bool moving;
+    //public Ease easingFunction = Ease.OutElastic;
+    public List<Vector3> anchors;
+
+    private Camera camera;
+	void Start () {
+        camera = GetComponent<Camera>();
+        camera.transform.position = anchors[index];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+            SetIndex((++index)%anchors.Count);
+	}
+
+    public void SetIndex(int i)
+    {
+        index = i;
+        camera.transform.DOMove(anchors[i], duration).SetEase(easingFunction);
+    }
+}
