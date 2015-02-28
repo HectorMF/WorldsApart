17aea27826830b792d4a2b09cf515618728677bf
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-02-28 12:55:43 -0500

    Farming Minigame
    Concept complete, will need more work later

--- Assets/Scripts/Games/Crops Minigame/CropsGameController.cs
+++ Assets/Scripts/Games/Crops Minigame/CropsGameController.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 using WorldsApart;
 using WorldsApart.Utility;
 using WorldsApart.Cameras;
+using System.Collections.Generic;
+using Vexe.Runtime.Types;
+using System;
 
 namespace WorldsApart.Games.CropsMinigame
 {
-    public class CropsGameController : MonoBehaviour
+    public class CropsGameController : BetterBehaviour
     {
         Vector3 previousPosition;
         public GameObject miniGame;
         public float speed = 20.0f;
         float lerpTime;
 
-        int scoreGoal;
+        public List<Hazard> Hazards;
+        public float spawnFrequency;
+        public BoundingBox bounds;
+
+        private float timePassed = 0f;
+
+        int scoreGoal = 5;
 
         public enum MiniGameState
         {
@@ -36,8 +46,25 @@ namespace WorldsApart.Games.CropsMinigame
             {
                 case MiniGameState.Starting:
                     miniGame.GetComponent<ObjectGenerator>().enabled = true;
+                    CurrentState = MiniGameState.Playing;
                     break;
                 case MiniGameState.Playing:
+                    timePassed += Time.deltaTime;
+                    if (timePassed > spawnFrequency)
+                    {
+                        float sum = Hazards.Sum(x => x.probability);
+                        float selection = UnityEngine.Random.Range(0f, sum);
+                        for (int i = 0; i < Hazards.Count; i++)
+                        {
+                            selection -= Hazards[i].probability;
+                            if (selection <= 0)
+                            {
+                                Vector3 spawnLocation = bounds.getRandomOutOfBounds(transform.position, BoundingBox.Axis.Horizontal);
+                                Instantiate(Hazards[i].prefab, spawnLocation, Quaternion.identity);
+                            }
+                        }
+                        timePassed = 0f;
+                    }
                     if (CounterManager.Instance.GetCounter("GardenMiniGame").count >= scoreGoal)
                     {
                         CurrentState = MiniGameState.Ending;
@@ -67,6 +94,13 @@ namespace WorldsApart.Games.CropsMinigame
             return ThirdWorldManager.Instance.AnyWater
                 && gameObject.GetComponent<Thirst>().AmountDrank < gameObject.GetComponent<Thirst>().TotalRequiredWater
                     && ThirdWorldManager.Instance.TryAction();
+        }
+
+        [Serializable]
+        public class Hazard
+        {
+            public GameObject prefab;
+            public float probability;
         }
     }
 }
