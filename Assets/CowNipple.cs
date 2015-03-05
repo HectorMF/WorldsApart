c97ecfcd9015a663dad4788fe59753ed8846333a
Hector Medina-Fetterman <javi@medina.net>
2015-03-05 14:21:14 -0500

    Merging nipple dragger and nipple filler scripts

--- Assets/CowNipple.cs
+++ Assets/CowNipple.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace GoofyGhost.WorldsApart
 {
-    public class NippleDragger : MonoBehaviour, IDraggable
+    public class CowNipple : MonoBehaviour, IDraggable
     {
         public bool canDrag = true;
         public Ease easingFunction = Ease.OutElastic;
@@ -17,13 +17,23 @@ namespace GoofyGhost.WorldsApart
         public float yOffsetMin;
         public float yOffsetMax;
 
+        public static float minFillValue = 0;
+        public static float maxFillValue = 1;
+
+        public float currentFillValue;
+        public float fillRate = .1f;
+        public Vector3 minScale;
+        public Vector3 maxScale;
+
         private Vector3 position;
         private Vector3 screenPoint;
         private Vector3 offset;
 
         public void Start()
         {
+
             DragHandler.Subscribe(this);
+            currentFillValue = UnityEngine.Random.Range(minFillValue, maxFillValue);
             CanDrag = true;
         }
 
@@ -38,8 +48,10 @@ namespace GoofyGhost.WorldsApart
         public void EndDrag()
         {
             transform.DOMove(position, tweenDuration).SetEase(easingFunction).OnComplete(SetCanDrag);
+
             CanDrag = false;
         }
+
         private void SetCanDrag()
         {
             this.CanDrag = true;
@@ -53,13 +65,30 @@ namespace GoofyGhost.WorldsApart
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
             float yPosition = Clamp(curPosition.y, yOffsetMin + position.y, yOffsetMax + position.y);
             transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
+            transform.localScale = Vector3.Lerp(minScale, maxScale, (yPosition - (yOffsetMin + position.y)) / ((position.y) - (yOffsetMin + position.y)));
+
+            //Debug.og
+
             if (yPosition == yOffsetMax + position.y)
             {
                 DragHandler.StopDragging();
             }
-            //if (yPosition == yOffsetMin + position.y)
-            //    DragHandler.StopDragging();
-            
+            if (yPosition == yOffsetMin + position.y)
+            {
+                currentFillValue = 0;
+            }
+
+        }
+
+        void Update()
+        {
+            if (currentFillValue == maxFillValue) return;
+
+            currentFillValue += fillRate * Time.deltaTime;
+            currentFillValue = Clamp(currentFillValue, minFillValue, maxFillValue);
+            transform.localScale = Vector3.Lerp(minScale, maxScale, (currentFillValue - minFillValue) / (maxFillValue - minFillValue));
+            if (currentFillValue == maxFillValue)
+                transform.DOShakeScale(.5f, new Vector3(.12f, 0, 0), 16);
         }
 
         public float Clamp(float val, float min, float max)
