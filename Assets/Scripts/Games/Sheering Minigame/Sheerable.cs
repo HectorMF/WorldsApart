37a8cde4455d9d47f9354482b166925e0e8307b9
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-03-06 18:58:43 -0500

    Sheep game changes

--- Assets/Scripts/Games/Sheering Minigame/Sheerable.cs
+++ Assets/Scripts/Games/Sheering Minigame/Sheerable.cs
@@ -6,72 +6,40 @@ using UnityEngine;
 
 namespace WorldsApart.Games.SheeringMinigame
 {
+    [RequireComponent (typeof (SpriteRenderer))]
     public class Sheerable : MonoBehaviour
     {
         private SheeringController controller;
 
-        bool sheered;
         public int index;
-
-        //public void AssignAdjacent(Direction dir, Sheerable sheerable)
-        //{
-        //    switch(dir)
-        //    {
-        //        case Direction.Down:
-        //            down = sheerable;
-        //            break;
-        //        case Direction.Left:
-        //            left = sheerable;
-        //            break;
-        //        case Direction.Right:
-        //            right = sheerable;
-        //            break;
-        //        case Direction.Up:
-        //            up = sheerable;
-        //            break;
-        //    }
-        //}
 
         public void setController(SheeringController controller)
         {
             this.controller = controller;
         }
 
-        //public Sheerable GetAdjacent(Direction dir)
-        //{
-        //    switch (dir)
-        //    {
-        //        case Direction.Down:
-        //            return down;
-        //        case Direction.Left:
-        //            return left;
-        //        case Direction.Right:
-        //            return right;
-        //        case Direction.Up:
-        //            return up;
-        //    }
-
-        //    throw new Exception("Invalid Direction");
-        //}
-
         void OnMouseDown()
         {
             if (controller.ValidIndex(index))
             {
-                var renderer = gameObject.GetComponent<MeshRenderer>();
-                if (renderer != null) renderer.material.color = Color.red;
-                controller.registerSelected(index);
+                Sheer();
                 controller.setMouseActive(true);
             }
+        }
+
+        private void Sheer()
+        {
+            var sprite = gameObject.GetComponent<SpriteRenderer>();
+            sprite.sprite = controller.getSheeredSprite();
+
+            controller.registerSelected(index);
         }
 
         void OnMouseOver()
         {
             if (controller.getMouseActive() && controller.ValidIndex(index))
             {
-                var renderer = gameObject.GetComponent<MeshRenderer>();
-                if (renderer != null) renderer.material.color = Color.red;
-                controller.registerSelected(index);
+                Sheer();
             }
         }
 
@@ -80,12 +48,4 @@ namespace WorldsApart.Games.SheeringMinigame
             controller.setMouseActive(false);
         }
     }
-
-    //public enum Direction
-    //{
-    //    Left,
-    //    Right,
-    //    Up,
-    //    Down
-    //}
 }
