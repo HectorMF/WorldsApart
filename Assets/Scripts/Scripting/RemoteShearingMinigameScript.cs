f3bbcf3c5f90607c66ccfa8af28f03803ab9d12c
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-03-06 20:39:19 -0500

    more sheep stuff

---
+++ Assets/Scripts/Scripting/RemoteShearingMinigameScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WorldsApart.Scripting
+{
+	public class RemoteShearingMinigameScript : RemoteScript
+	{
+		public override void Start(System.Action OnFinish)
+		{
+			Debug.Log("Shearing game!");
+			ThirdWorldManager.Instance.UsedAction();
+			Fader.FadeToBlack(0,2,"Shear the Sheep!!","Don't touch any space twice!",switchLevel);
+			base.targetController.scripts.Clear();
+			OnFinish();
+		}
+
+		public void switchLevel(){
+			Application.LoadLevel("Shearing Minigame");
+		}
+	}
+}
