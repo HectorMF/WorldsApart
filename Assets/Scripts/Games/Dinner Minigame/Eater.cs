b8b5331e43cc4dd854e4b4d5a68461dd002cb00e
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-03-07 14:22:39 -0500

    Dinner Mini-game basic concept

---
+++ Assets/Scripts/Games/Dinner Minigame/Eater.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace WorldsApart.Games.DinnerMinigame
+{
+    public class Eater : MonoBehaviour
+    {
+        public Plate plate;
+        public float time;
+        private float timePassed;
+
+        private const int maxMood = 5;
+        private int mood;
+        private MeshRenderer renderer;
+
+        void Start()
+        {
+            mood = maxMood;
+            renderer = GetComponent<MeshRenderer>();
+            timePassed = time;
+        }
+
+        void Update()
+        {
+            timePassed -= Time.deltaTime;
+
+            if (timePassed <= 0)
+            {
+                if (plate.Eat())
+                {
+                    if (mood < maxMood) mood++;
+                }
+                else if (mood > 0) mood--;
+                timePassed = time;
+            }
+
+            renderer.material.color = (Color.blue / maxMood) * mood;
+        }
+    }
+}
---
+++ Assets/Scripts/Games/Dinner Minigame/Plate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace WorldsApart.Games.DinnerMinigame
+{
+    public class Plate : MonoBehaviour
+    {
+        private const int filledServings = 5;
+        private int Servings;
+        private MeshRenderer renderer;
+
+        void Start()
+        {
+            Servings = filledServings;
+            renderer = GetComponent<MeshRenderer>();
+            renderer.material.color = Color.red;
+        }
+
+        public bool Eat()
+        {
+            if (Servings == 0) return false;
+            else
+            {
+                renderer.material.color -= Color.red / filledServings;
+                Servings--;
+                return true; 
+            }
+        }
+
+        void OnMouseUpAsButton()
+        {
+            Servings = filledServings;
+            renderer.material.color = Color.red;
+        }
+    }
+}
