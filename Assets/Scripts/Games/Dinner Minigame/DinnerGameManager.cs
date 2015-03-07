c906c0c3877862aa08d2393badc1902a6b8cb90a
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-03-07 15:19:50 -0500

    Changes to wobble and dinner game

---
+++ Assets/Scripts/Games/Dinner Minigame/DinnerGameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace WorldsApart.Games.DinnerMinigame
+{
+    public class DinnerGameManager : MonoBehaviour
+    {
+        public List<Sprite> moods;
+
+        public Sprite getMoodSprite(int mood)
+        {
+            return moods[mood];
+        }
+    }
+}
