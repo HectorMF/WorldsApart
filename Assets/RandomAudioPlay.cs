8c1d078aa79f407303ef48d88611d11ec0542074
Hector Medina-Fetterman <javi@medina.net>
2015-03-13 17:07:41 -0400

    More sounds and random player script

---
+++ Assets/RandomAudioPlay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomAudioPlay : MonoBehaviour {
+	public bool playOnStart;
+	public float minTime;
+	public float maxTime;
+
+	private float timer;
+	private float nextPlay;
+	private AudioSource audio;
+
+	void Start (){
+		nextPlay = Random.Range(minTime, maxTime);
+		audio = GetComponent<AudioSource>();
+		if(playOnStart)
+			audio.Play();
+	}
+
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer >= nextPlay)
+		{
+			timer -= nextPlay;
+			nextPlay = Random.Range(minTime, maxTime);
+			audio.Play();
+		}
+	}
+}
