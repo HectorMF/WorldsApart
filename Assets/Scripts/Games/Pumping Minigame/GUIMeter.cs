008b9db31176c4f520d69105a3e643b8c9084e1b
Ed Grundner <eddiee.grundner@gmail.com>
2015-03-19 19:30:23 -0400

    Changed the pumping minigame added the remote pumping script to worlds apart again scene

---
+++ Assets/Scripts/Games/Pumping Minigame/GUIMeter.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIMeter : MonoBehaviour 
+{
+	public float speed = 0.1f;
+
+	private int difficulty;
+	private float oldValue;
+	private float currentValue;
+	private Slider slider;
+	private ScoreController scoreController;
+	private bool draining;
+	private float drainSpeed;
+	private PumpAnimator pumpAnim;
+	
+	void Start () 
+	{
+		GameObject scoreObject = GameObject.Find("ScoreController");
+		if(scoreObject != null)
+			scoreController = scoreObject.GetComponent<ScoreController>();
+
+		slider = this.GetComponent<Slider>();
+		if (slider != null){
+			slider.maxValue = 3;
+			slider.value = slider.minValue;
+		}
+		currentValue = slider.value;
+		oldValue = slider.value;
+		difficulty = 1;
+		draining = false;
+		drainSpeed = 2 * -speed;
+		pumpAnim = GetComponentInChildren<PumpAnimator>();
+
+	}
+
+	void Update () 
+	{
+		if(slider.value == slider.maxValue || slider.value == slider.minValue)
+			speed = -speed;
+
+		if (!draining && (Input.touchCount > 0 || Input.GetMouseButtonDown(0)))
+		{
+			draining = true;
+			pumpAnim.Pump();
+			//if(scoreController != null)
+			//	scoreController.Water += slider.value;
+			ThirdWorldManager.Instance.IncrementWater(Mathf.FloorToInt(slider.value));
+		}
+		else if (draining)
+		{
+			speed = Mathf.Abs(speed);
+			slider.value += drainSpeed * Time.deltaTime;
+			if (slider.value == slider.minValue) draining = false;
+		}
+		else 
+			slider.value += speed * Time.deltaTime * difficulty;
+
+	}
+}
---
+++ Assets/Scripts/Games/Pumping Minigame/PumpingMinigame.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class PumpingMinigame : MonoBehaviour {
+	public Text timer;
+
+	private enum State {Starting, Started, Finishing, Finished}
+	private State currentState;
+
+	private float countDownTime = 6f;
+	private float playTime = 20f;
+
+	private int minutes;
+	private int seconds;
+	private int oldSeconds;
+
+	private GUIMeter meterScript;
+
+	void Start ()
+	{
+		Fader.FadeToClear(2,2,"Pump Water", "Time pumps for max water");
+		currentState = State.Starting;
+		meterScript = GetComponentInChildren<GUIMeter>();
+		meterScript.enabled = false;
+	}
+
+	void Update ()
+	{
+		switch (currentState)
+		{
+		case State.Starting:
+			if (countDownTime <= 0) {
+				currentState = State.Started;
+				meterScript.enabled = true;
+			}
+			else 
+				countDownTime = UpdateTimer(countDownTime);
+			break;
+		case State.Started:
+			if (playTime <= 0){
+				currentState = State.Finishing;
+			}
+			else 
+				playTime = UpdateTimer(playTime);
+			break;
+		case State.Finishing:
+			Fader.FadeToBlack(0,2,"Game Over");
+			meterScript.enabled = false;
+			currentState = State.Finished;
+			break;
+		case State.Finished:
+			this.enabled = false;
+			//Application.LoadLevel("WorldsApartAgain");
+			break;
+		}
+	}
+
+	private float UpdateTimer (float time) 
+	{
+		oldSeconds = seconds;
+
+		time -= Time.deltaTime;
+		minutes = (int)(time / 60);
+		seconds = (int)(time % 60);
+		
+		//instead of updating every frame, update every second change
+		if (seconds != oldSeconds)
+		{
+			timer.text = minutes + ":" + seconds.ToString("00");
+			
+			if (minutes == 0 && seconds <= 10)
+			{
+				timer.DOColor(Color.red, .5f).SetLoops(2, LoopType.Yoyo);
+				timer.gameObject.transform.DOScale(new Vector3(1.5f, 1.5f, 1), .5f).SetLoops(2, LoopType.Yoyo);
+			}
+		}
+		return time;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
