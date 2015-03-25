747484b67fb57b567cbd7a1949f3f16f1cb073ab
Ryan Bone <rbone@covermymeds.com>
2015-03-25 07:54:17 -0400

    all the water :ocean:

---
+++ Assets/Scripts/Scripting/RemoteFillTroughScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace WorldsApart.Scripting
+{
+	public class RemoteFillTroughScript : RemoteScript 
+	{
+		public GameObject selfGameObject;
+		TroughManager tm;
+
+		public override void Start(Action OnFinish)
+		{
+			Debug.Log("water the trough");
+			tm = selfGameObject.GetComponent<TroughManager>();
+
+			if (CanAndShouldWaterTrough())
+			{
+				tm.AddWaterToTrough();
+			}
+			ThirdWorldManager.Instance.Report();
+
+			OnFinish();
+		}
+		
+		bool CanAndShouldWaterTrough()
+		{
+			return ThirdWorldManager.Instance.AnyWater && (ThirdWorldManager.Instance.Actions > 0) && tm.NeedsWater();
+		}
+	}
+}
