240d83335457f22e7f7fa516658cabb0924631f1
Ryan Bone <rbone@covermymeds.com>
2015-03-26 19:59:53 -0400

    more info pop up stuff and game handlers

---
+++ Assets/Scripts/Handlers/ThirdWorldHandlers/DinnerGameHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WorldsApart.Handlers
+{
+	public class DinnerGameHandler : Handler
+	{
+		public override void innerDelegate()
+		{
+			Debug.Log("Dinner game!");
+			ThirdWorldManager.Instance.UsedAction();
+			Fader.FadeToBlack(0,2,"Feed your Family!!","Touch the plates!", switchLevel);
+			gameObject.SetActive(false);
+		}
+		
+		public void switchLevel(){
+			Application.LoadLevel("Dinner Minigame");
+		}
+	}
+}
