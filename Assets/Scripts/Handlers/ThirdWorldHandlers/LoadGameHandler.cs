3193aafe489c36655b95709331c10fe5181f06c1
Ryan Bone <rbone@covermymeds.com>
2015-03-26 20:33:56 -0400

    create load game handler

---
+++ Assets/Scripts/Handlers/ThirdWorldHandlers/LoadGameHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WorldsApart.Handlers
+{
+	public class LoadGameHandler : Handler
+	{
+		public string GameName, Title, Subtitle;
+
+		public override void innerDelegate()
+		{
+			ThirdWorldManager.Instance.UsedAction();
+			Fader.FadeToBlack(0, 2, Title, Subtitle, switchLevel);
+			gameObject.SetActive(false);
+		}
+		
+		public void switchLevel(){
+			Application.LoadLevel(GameName);
+		}
+	}
+}
