adba4f8cf5b7de5494a92d340903177254c0715b
Hector Medina-Fetterman <javi@medina.net>
2015-04-02 17:50:17 -0400

    Fader for main scene

---
+++ Assets/Scripts/MainSceneScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Fader.FadeToClear(Fader.Gesture.None,2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
