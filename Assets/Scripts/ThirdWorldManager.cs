961dee1f8a9aa3770c7f13a3ddeb190ec99b4bdb
navidmatin <seyedmatin.1@osu.edu>
2015-04-09 19:15:07 -0400

    Did some clean up on how the ThirdWorldManager looks, and made the Food and Water read only (you need to call increment, or decrement to increase or decrease food or water

--- Assets/Scripts/ThirdWorldManager.cs
+++ Assets/Scripts/ThirdWorldManager.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 using WorldsApart.Handlers;
 
+
 public class ThirdWorldManager 
 {
-	private static ThirdWorldManager instance;
 
+    public enum Weather
+    {
+        Rainy,
+        Nice,
+        Dry,
+    }
 	public delegate void DayEnd();
 	public static event DayEnd OnDayEnd;
 
@@ -15,6 +21,122 @@ public class ThirdWorldManager
     public delegate void HasPackChanged(int value);
     public static event HasPackChanged OnHasPackChanged;
 
+    private Mood mood = new Mood();
+    private Weather currentWeather;
+    private int requiredFood;
+    private int currentFood, actions, availableWater;
+    private int prevWater = 0;
+    private int _currentWater;
+
+    #region Properties
+    public int WaterCapacity = 20;
+    public Weather CurrentWeather { get { return currentWeather; } set { } }
+    public Mood.MoodNames CurrentMood { get { return mood.CurrentMood; } set { } }
+    public int CurrentWater { get { return currentWater; } }
+    public int CurrentFood { get { return currentFood; }}
+    public int AvailableWater { get { return availableWater; } set { } }
+    public int RequiredFood { get { return requiredFood; } set { requiredFood = value; } }
+    public int Actions { get { return actions; } set { } }
+    public bool AnyWater { get { return currentWater > 0; } set { } }
+
+    private int currentWater
+    {
+        get { return _currentWater; }
+        set
+        {
+            prevWater = _currentWater;
+            _currentWater = value;
+            if (ShouldSwapSprite()) InvokeSpriteSwap();
+        }
+    }
+
+    #endregion
+
+    #region SingeltonStuff
+    private static ThirdWorldManager instance;
+    private ThirdWorldManager()
+    {
+        Reinitialize();
+        Report();
+    }
+
+    public static ThirdWorldManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ThirdWorldManager();
+            }
+            return instance;
+        }
+    }
+    #endregion endOfSingeltonStuff
+
+    #region PrivateMethods
+    private void Reinitialize()
+    {
+        currentWater = currentFood = 0;
+        float rand = Random.Range(0.0f, 1.0f);
+        if (rand < 0.1f)
+        {
+            currentWeather = Weather.Rainy;
+            availableWater = Random.Range(35, 45);
+            IncrementMood();
+            actions = (int)CurrentMood;
+        }
+        else if (rand < 0.7f)
+        {
+            currentWeather = Weather.Nice;
+            availableWater = Random.Range(25, 35);
+            actions = (int)CurrentMood;
+        }
+        else
+        {
+            currentWeather = Weather.Dry;
+            availableWater = Random.Range(15, 25);
+            DecrementMood();
+            actions = (int)CurrentMood;
+        }
+
+        if (OnNewWeather != null) OnNewWeather(currentWeather);
+
+        UnityEngine.Debug.Log("A new day! The weather is " + currentWeather);
+    }
+    private void ResolveDay()
+    {
+        if (currentFood >= requiredFood)
+        {
+            Debug.Log("You fed your family!");
+            IncrementMood();
+        }
+        else
+        {
+            DecrementMood();
+            Debug.Log("Your family is hungry");
+        }
+
+        Report();
+        if (OnDayEnd != null) OnDayEnd();
+        Reinitialize();
+    }
+
+
+    bool ShouldSwapSprite()
+    {
+        return (prevWater == 0 && _currentWater > 0) || (prevWater > 0 && _currentWater == 0);
+    }
+
+    void InvokeSpriteSwap()
+    {
+        var handler = new SetAnimationBoolHandler();
+        handler.gameObject = GameObject.Find("MainChar");
+        handler.booleanName = "hasWater";
+        handler.value = _currentWater > 0;
+        handler.Invoke();
+    }
+    #endregion
+    
 
     private bool _hasPack;
     public bool HasPack
@@ -35,106 +157,6 @@ public class ThirdWorldManager
         }
     }
 
-	public enum Weather 
-	{
-		Rainy,
-		Nice,
-		Dry,
-	}
-
-	public int WaterCapacity = 20;
-	public Weather CurrentWeather { get { return currentWeather; } set {} }
-	public Mood.MoodNames CurrentMood { get { return mood.CurrentMood; }  set {} }
-	public int CurrentWater { get { return currentWater; }  set {} }
-	public int CurrentFood { get { return currentFood; }  set {} }
-	public int AvailableWater { get { return availableWater; }  set {} }
-	public int RequiredFood { get { return requiredFood; }  set { requiredFood = value; } }
-	public int Actions { get { return actions; }  set {} }
-	public bool AnyWater { get { return currentWater > 0; } set {} }
-
-	private Mood mood = new Mood();
-	private Weather currentWeather;
-	private int requiredFood;
-	private int currentFood, actions, availableWater;
-    private int prevWater = 0;
-    private int _currentWater;
-    private int currentWater
-    {
-        get { return _currentWater; }
-        set
-        {
-			prevWater = _currentWater;
-            _currentWater = value;
-            if (ShouldSwapSprite ()) InvokeSpriteSwap ();
-        }
-    }
-
-	public ThirdWorldManager()
-	{
-		Reinitialize();
-		Report();
-	}
-	
-	public static ThirdWorldManager Instance
-	{
-		get 
-		{
-			if (instance == null)
-			{
-				instance = new ThirdWorldManager();
-			}
-			return instance;
-		}
-	}
-	
-	private void Reinitialize()
-	{
-		currentWater = currentFood = 0;
-		float rand = Random.Range(0.0f, 1.0f);
-		if (rand < 0.1f)
-		{  
-			currentWeather = Weather.Rainy;
-			availableWater = Random.Range (35, 45);
-			IncrementMood();
-			actions = (int)CurrentMood;
-		}
-		else if (rand < 0.7f)
-		{
-			currentWeather = Weather.Nice;
-			availableWater = Random.Range(25, 35);
-			actions = (int)CurrentMood;
-		}
-		else
-		{
-			currentWeather = Weather.Dry;
-			availableWater = Random.Range(15, 25);
-			DecrementMood();
-			actions = (int)CurrentMood;
-		}
-
-		if (OnNewWeather != null) OnNewWeather(currentWeather);
-
-		UnityEngine.Debug.Log ("A new day! The weather is " + currentWeather);
-	}
-
-	private void ResolveDay()
-	{
-		if (currentFood >= requiredFood) 
-		{ 
-			Debug.Log ("You fed your family!");
-			IncrementMood();
-		}
-		else
-		{
-			DecrementMood();
-			Debug.Log("Your family is hungry");
-		}
-
-		Report();
-		if(OnDayEnd != null) OnDayEnd();
-		Reinitialize();
-	}
-	
 	public void GetWater()
 	{
 		if(availableWater > 0)
@@ -213,17 +235,4 @@ public class ThirdWorldManager
 		Debug.Log("-----------------------------------------");
 	}
 
-	bool ShouldSwapSprite ()
-	{
-		return (prevWater == 0 && _currentWater > 0) || (prevWater > 0 && _currentWater == 0);
-	}
-	
-	void InvokeSpriteSwap ()
-	{
-		var handler = new SetAnimationBoolHandler ();
-		handler.gameObject = GameObject.Find ("MainChar");
-		handler.booleanName = "hasWater";
-		handler.value = _currentWater > 0;
-		handler.Invoke ();
-	}
 }
