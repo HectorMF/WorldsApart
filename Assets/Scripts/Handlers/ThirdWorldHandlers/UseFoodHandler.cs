e90d7527c9d2a141a4a84eaeb256bdf91f708657
Ryan Bone <rbone@covermymeds.com>
2015-04-16 19:23:52 -0400

    add use food handler to 3rd house

---
+++ Assets/Scripts/Handlers/ThirdWorldHandlers/UseFoodHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using WorldsApart.Clickables;
+using WorldsApart.Scripting;
+
+namespace WorldsApart.Handlers
+{
+	public class UseFoodHandler : Handler 
+	{
+		public GameObject eaterPopUp;
+		
+		public override void innerDelegate()
+		{
+			if (CanEat())
+			{
+				RemoteScriptHandler rsh = (RemoteScriptHandler)eaterPopUp.GetComponent<Clickable>().Handlers[0];
+				RemoteOpenPopUpScript ropus = (RemoteOpenPopUpScript)rsh.scripts[0];
+				ThirdWorldManager.Instance.DecrementFood(ropus.RequiredFood);
+			}
+			ThirdWorldManager.Instance.Report();
+		}
+		
+		bool CanEat()
+		{
+			return ThirdWorldManager.Instance.CurrentFood > 0;
+		}
+	}
+}
