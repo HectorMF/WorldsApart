cad7fa28c1809514ce1e5f0400c71e2c0d9d63c5
Ryan Bone <rbone@covermymeds.com>
2015-04-16 19:24:16 -0400

    create script for resetting popups on day end

---
+++ Assets/SetupOnDayEnd.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupOnDayEnd : MonoBehaviour {
+
+	GameObject Info, Action;
+	// Use this for initialization
+	void Start () {
+		Info = transform.Find ("Info").gameObject;
+		Action = transform.Find ("Action").gameObject;
+	}
+	
+	void OnEnable()
+	{
+		ThirdWorldManager.OnDayEnd += DayEnd;
+	}
+	
+	void OnDisable()
+	{
+		ThirdWorldManager.OnDayEnd -= DayEnd;
+	}
+
+	public virtual void DayEnd()
+	{
+		Info.SetActive (false);
+		Action.SetActive (true);
+	}
+}
