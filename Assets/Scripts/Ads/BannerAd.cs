deb3099b72a8e02fa49142df081e822af4105c24
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-04-23 19:42:14 -0400

    Banner disappear on swipe

--- Assets/Scripts/Ads/BannerAd.cs
+++ Assets/Scripts/Ads/BannerAd.cs
@@ -4,27 +4,27 @@ using GoogleMobileAds.Api;
 using System;
 
 public class BannerAd : MonoBehaviour {
-
+    public static BannerView banner;
 	// Use this for initialization
 	void Start () {
         #if UNITY_ANDROID
             string adUnitId = "ca-app-pub-1850231283632979/2985243049";
         #elif UNITY_IPHONE
-        string adUnitId = "ca-app-pub-1850231283632979/2845642242";
+            string adUnitId = "ca-app-pub-1850231283632979/2845642242";
         #else
             string adUnitId = "unexpected_platform";
         #endif
 
         // Create a 320x50 banner at the top of the screen.
-        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        banner = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
 
-        bannerView.AdLoaded += HandleAdLoaded;
+        banner.AdLoaded += HandleAdLoaded;
 
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).Build();
         // Load the banner with the request.
-        bannerView.LoadAd(request);
-        bannerView.Show();
+        banner.LoadAd(request);
+        banner.Show();
 	}
 
     public void HandleAdLoaded(object sender, EventArgs args)
